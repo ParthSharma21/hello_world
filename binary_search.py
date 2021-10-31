d1384a1420eedea50e14ee10c367d37764e77da1
rohitmsd1819 <72183909+rohitmsd1819@users.noreply.github.com>
2021-10-31 23:38:46 +0530

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,6 +1,6 @@
 #this is a binary search program
 #yes this program is correct
-#no this program is not correct
+#this program is correct
 def binary_search(arr, low, high, x): 
   
     # Check base case 
